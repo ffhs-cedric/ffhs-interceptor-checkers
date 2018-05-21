00eacc3034c26838b1a72c3ca6f20cbba5f2057e
ffhs-cedric <cedric.muehlebach@students.ffhs.ch>
2018-05-21 18:06:53 +0200

    Add javadoc

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,10 +1,21 @@
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Field of the checkers board game. A field has a x/y position and a color. A field can have a
+ * brick set or not.
+ */
 class Field extends Rectangle {
 
   private Brick brick;
 
+  /**
+   * Constructor to create a new field object.
+   *
+   * @param x Position x
+   * @param y Position y
+   * @param black Color switcher, black is black, !black is white
+   */
   Field(int x, int y, boolean black) {
     setWidth(Checkers.GRID_SIZE);
     setHeight(Checkers.GRID_SIZE);
@@ -12,10 +23,11 @@ class Field extends Rectangle {
     setFill(black ? Color.valueOf("black") : Color.valueOf("white"));
   }
 
-  public Brick getBrick() {
-    return brick;
-  }
-
+  /**
+   * Set a brick object on to the field
+   *
+   * @param brick Brick object to set on to the field
+   */
   void setBrick(Brick brick) {
     this.brick = brick;
   }
