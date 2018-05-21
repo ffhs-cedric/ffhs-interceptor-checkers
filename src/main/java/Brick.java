5776041c8082cd9bf932361718e251ca2b535853
ffhs-cedric <cedric.muehlebach@students.ffhs.ch>
2018-05-21 17:27:03 +0200

    Extract brick displacement to access it from the main application

--- src/main/java/Brick.java
+++ src/main/java/Brick.java
@@ -6,20 +6,12 @@ class Brick extends Pane {
   private double xPos, yPos, mouseX, mouseY;
 
   Brick(Color color, int x, int y) {
-
-    double radius = 0.33 * Checkers.GRID_SIZE;
-
-    xPos = x * Checkers.GRID_SIZE + (radius / 2);
-    yPos = y * Checkers.GRID_SIZE + (radius / 2);
-
-    Circle brick = new Circle(radius, color);
-    brick.setStroke(Color.BLACK);
-    brick.relocate(xPos, yPos);
+    Circle brick = new Circle(0.33 * Checkers.GRID_SIZE, color);
+    move(x, y);
     getChildren().add(brick);
 
     setOnMousePressed(
         e -> {
-          setFocused(true);
           mouseX = e.getSceneX();
           mouseY = e.getSceneY();
         });
@@ -29,8 +21,12 @@ class Brick extends Pane {
           double deltaX = e.getSceneX() - mouseX;
           double deltaY = e.getSceneY() - mouseY;
           relocate(deltaX + xPos, deltaY + yPos);
-          xPos = brick.getCenterX();
-          yPos = brick.getCenterY();
         });
+  }
+
+  void move(int x, int y) {
+    xPos = x * Checkers.GRID_SIZE + Checkers.GRID_SIZE / 2;
+    yPos = y * Checkers.GRID_SIZE + Checkers.GRID_SIZE / 2;
+    relocate(xPos, yPos);
   }
 }
