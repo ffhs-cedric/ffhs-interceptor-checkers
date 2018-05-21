394a96a428f773b27727bd805390ca03a4efa13f
ffhs-cedric <cedric.muehlebach@students.ffhs.ch>
2018-05-21 12:13:27 +0200

    Extend Field with Brick

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -3,10 +3,20 @@ import javafx.scene.shape.Rectangle;
 
 class Field extends Rectangle {
 
+  Brick brick;
+
   Field(int x, int y, boolean black) {
     setWidth(Checkers.GRID_SIZE);
     setHeight(Checkers.GRID_SIZE);
     relocate(x * Checkers.GRID_SIZE, y * Checkers.GRID_SIZE);
     setFill(black ? Color.valueOf("black") : Color.valueOf("white"));
+  }
+
+  public Brick getBrick() {
+    return brick;
+  }
+
+  public void setBrick(Brick brick) {
+    this.brick = brick;
   }
 }
